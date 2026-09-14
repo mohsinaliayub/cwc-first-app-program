810d0003241e39253dab272cbc041140af9839b1
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2026-09-15 01:24:29 +0200

    add project files for WarCard game

---
+++ 01-ios-foundations/01-war-card/WarCard/WarCard/WarCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardApp.swift
+//  WarCard
+//
+//  Created by Mohsin Ali Ayub on 15.09.26.
+//
+
+import SwiftUI
+
+@main
+struct WarCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
