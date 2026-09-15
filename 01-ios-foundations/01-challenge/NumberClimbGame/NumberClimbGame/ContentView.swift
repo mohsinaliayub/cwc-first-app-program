77fbaee13a0f9ddcc466fa376780224612cce983
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2026-09-16 00:48:52 +0200

    finish the challenge

--- 01-ios-foundations/01-challenge/NumberClimbGame/NumberClimbGame/ContentView.swift
+++ 01-ios-foundations/01-challenge/NumberClimbGame/NumberClimbGame/ContentView.swift
@@ -8,14 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var number = 0
+    @State private var shouldIncrease = true
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("\(number)")
+                .font(.largeTitle)
+            Button("Tap Me!") {
+                increaseOrDecrease()
+            }
         }
         .padding()
+    }
+    
+    func increaseOrDecrease() {
+        if shouldIncrease {
+            increase()
+        } else {
+            decrease()
+        }
+        
+        // if number goes above 50, start decreasing.
+        // If number goes below 0, start increasing.
+        if number > 50 {
+            shouldIncrease = false
+        }
+        if number < 0 {
+            shouldIncrease = true
+        }
+    }
+    
+    func increase() {
+        number += randomNumberBetween1And10()
+    }
+    
+    func decrease() {
+        number -= randomNumberBetween1And10()
+    }
+    
+    func randomNumberBetween1And10() -> Int {
+        Int.random(in: 1...10)
     }
 }
 
