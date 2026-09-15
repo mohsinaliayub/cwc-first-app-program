8dc2731f3074c9a4de5d08dd228db4d131f43aa5
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2026-09-16 01:02:45 +0200

    add image assets

--- 01-ios-foundations/02-sushi-menu/SushiMenu/SushiMenu/MenuView.swift
+++ 01-ios-foundations/02-sushi-menu/SushiMenu/SushiMenu/MenuView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  MenuView.swift
 //  SushiMenu
 //
 //  Created by Mohsin Ali Ayub on 16.09.26.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MenuView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    MenuView()
 }
