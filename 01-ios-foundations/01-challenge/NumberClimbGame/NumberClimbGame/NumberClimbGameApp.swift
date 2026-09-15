f6087e418b5c7f10e61082c1daa02485eeac0fa5
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2026-09-16 00:38:24 +0200

    add project for first challenge

---
+++ 01-ios-foundations/01-challenge/NumberClimbGame/NumberClimbGame/NumberClimbGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumberClimbGameApp.swift
+//  NumberClimbGame
+//
+//  Created by Mohsin Ali Ayub on 16.09.26.
+//
+
+import SwiftUI
+
+@main
+struct NumberClimbGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
