8ef1bf8779e752683422ba9d2a0633112a6b072c
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2026-09-16 01:00:26 +0200

    create project for sushi menu app

---
+++ 01-ios-foundations/02-sushi-menu/SushiMenu/SushiMenu/SushiMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SushiMenuApp.swift
+//  SushiMenu
+//
+//  Created by Mohsin Ali Ayub on 16.09.26.
+//
+
+import SwiftUI
+
+@main
+struct SushiMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
