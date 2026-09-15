ee924f07ed2341334a10f87b1264965242bf3830
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2026-09-15 12:36:59 +0200

    display caption for the image

--- 01-ios-foundations/01-war-card/WarCard/WarCard/ContentView.swift
+++ 01-ios-foundations/01-war-card/WarCard/WarCard/ContentView.swift
@@ -9,11 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Image("niagarafalls")
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-            .clipShape(RoundedRectangle(cornerRadius: 15))
-        
+        VStack(alignment: .leading, spacing: 12) {
+            Image("niagarafalls")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .clipShape(RoundedRectangle(cornerRadius: 15))
+            
+            HStack {
+                Text("Niagara Falls")
+                    .font(.title)
+                    .fontWeight(.bold)
+            }
+            
+            Text("Come visit the falls for an experience of a lifetime.")
+        }
+        .padding()
     }
 }
 
