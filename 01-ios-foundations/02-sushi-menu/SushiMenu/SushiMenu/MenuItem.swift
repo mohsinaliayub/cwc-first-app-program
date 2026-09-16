ddc5873f3e18fb3f88d81ecaf8cabc72a48600f2
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2026-09-16 14:19:57 +0200

    fix the data model for ease of use

--- 01-ios-foundations/02-sushi-menu/SushiMenu/SushiMenu/MenuItem.swift
+++ 01-ios-foundations/02-sushi-menu/SushiMenu/SushiMenu/MenuItem.swift
@@ -12,7 +12,7 @@ struct MenuItem {
     /// The name to represent the sushi dish.
     let name: String
     /// The price of sushi dish in dollars rounded to two decimal digits.
-    let price: Double
+    let price: String
     /// The image name of the sushi dish in the Assets catalog.
     let imageName: String
 }
