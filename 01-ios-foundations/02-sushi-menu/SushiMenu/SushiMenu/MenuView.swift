2c79e6b41fcf701ca14c42cf250cf707d03f0f8e
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2026-09-16 15:51:29 +0200

    refactor shushi menu item data in separate file

--- 01-ios-foundations/02-sushi-menu/SushiMenu/SushiMenu/MenuView.swift
+++ 01-ios-foundations/02-sushi-menu/SushiMenu/SushiMenu/MenuView.swift
@@ -8,20 +8,8 @@
 import SwiftUI
 
 struct MenuView: View {
-    var menuItems = [
-        MenuItem(name: "Onigiri", price: "1.99", imageName: "onigiri"),
-        MenuItem(name: "Meguro Sushi", price: "5.99", imageName: "meguro-sushi"),
-        MenuItem(name: "Tako Sushi", price: "4.99", imageName: "tako-sushi"),
-        MenuItem(name: "Avocado Maki", price: "2.99", imageName: "avocado-maki"),
-        MenuItem(name: "Tobiko Spicy Maki", price: "4.99", imageName: "tobiko-spicy-maki"),
-        MenuItem(name: "Salmon Sushi", price: "4.99", imageName: "salmon-sushi"),
-        MenuItem(name: "Hamachi Sushi", price: "6.99", imageName: "hamachi-sushi"),
-        MenuItem(name: "Kani Sushi", price: "3.99", imageName: "kani-sushi"),
-        MenuItem(name: "Tamago Sushi", price: "3.99", imageName: "tamago-sushi"),
-        MenuItem(name: "California Roll", price: "3.99", imageName: "california-roll"),
-        MenuItem(name: "Shrimp Sushi", price: "3.99", imageName: "shrimp-sushi"),
-        MenuItem(name: "Ikura Sushi", price: "5.99", imageName: "ikura-sushi")
-    ]
+    @State private var menuItems: [MenuItem] = []
+    let dataService = DataService()
     
     var body: some View {
         List(menuItems) { item in
@@ -43,6 +31,9 @@ struct MenuView: View {
             .listRowBackground(Color.brown.opacity(0.1))
         }
         .listStyle(.plain)
+        .onAppear {
+            menuItems = dataService.fetchMenuItems()
+        }
     }
 }
 
