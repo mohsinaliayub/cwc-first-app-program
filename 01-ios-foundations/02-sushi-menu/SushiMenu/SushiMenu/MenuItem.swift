d7163388996bd9ea1e1321b4088d77725d592b57
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2026-09-16 14:13:04 +0200

    create custom data type to represent a sushi item

---
+++ 01-ios-foundations/02-sushi-menu/SushiMenu/SushiMenu/MenuItem.swift
@@ -0,0 +1,14 @@
+//
+//  MenuItem.swift
+//  SushiMenu
+//
+//  Created by Mohsin Ali Ayub on 16.09.26.
+//
+
+import Foundation
+
+struct MenuItem {
+    let name: String
+    let price: Double
+    let imageName: String
+}
