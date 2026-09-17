07baab7173062359cc8f7ddf6387abb79ad35117
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2026-09-17 13:52:42 +0200

    create data model for Movie

---
+++ 01-ios-foundations/02-challenge/MovieList/MovieList/Movie.swift
@@ -0,0 +1,18 @@
+//
+//  Movie.swift
+//  MovieList
+//
+//  Created by Mohsin Ali Ayub on 17.09.26.
+//
+
+import Foundation
+
+/// Represents a box office movie.
+struct Movie {
+    /// A unique identifier for the movie.
+    let id: UUID = .init()
+    /// The release name for the movie.
+    let name: String
+    /// The name of the poster included in the Assets catalog.
+    let posterName: String
+}
