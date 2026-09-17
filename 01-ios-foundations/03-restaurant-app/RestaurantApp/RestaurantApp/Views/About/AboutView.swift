6bf1551bd8ed77af55c630cb49afb47c293fdfa7
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2026-09-17 15:28:17 +0200

    create about screen with scrollable content

--- 01-ios-foundations/03-restaurant-app/RestaurantApp/RestaurantApp/Views/About/AboutView.swift
+++ 01-ios-foundations/03-restaurant-app/RestaurantApp/RestaurantApp/Views/About/AboutView.swift
@@ -9,7 +9,32 @@ import SwiftUI
 
 struct AboutView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView {
+                VStack(alignment: .leading, spacing: 12) {
+                    Image("restaurant-inside")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                    
+                    Text("Sukiyabashi Jiro (すきやばし次郎, Sukiyabashi Jirō) is a sushi restaurant in Ginza, Chūō, Tokyo, owned by Jiro Ono. \nSukiyabashi Jiro was the first sushi restaurant to receive three stars from the Michelin Guide. It was removed from the Michelin Guide in November 2019 as it does not receive reservations from the general public, instead requiring reservations to be made through the concierge of a luxury hotel.")
+                    
+                    Image("map")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                    
+                    Text("""
+                         Tsukamoto Sogyo Building
+                         Basement Floor 1
+                         2-15, Ginza 4-chome
+                         Chūō, Tokyo
+                         """)
+                }
+                .padding(.horizontal)
+                .padding(.bottom)
+            }
+            .scrollIndicators(.hidden)
+            .navigationTitle("About")
+        }
     }
 }
 
