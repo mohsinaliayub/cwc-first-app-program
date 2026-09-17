6d0d6261fd41dfa1e24f9ef0f2f1d84f525739c9
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2026-09-17 13:45:06 +0200

    create project for MovieList challenge

---
+++ 01-ios-foundations/02-challenge/MovieList/MovieList/MovieListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieListApp.swift
+//  MovieList
+//
+//  Created by Mohsin Ali Ayub on 17.09.26.
+//
+
+import SwiftUI
+
+@main
+struct MovieListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
