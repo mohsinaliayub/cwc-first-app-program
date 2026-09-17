323210c2c2fe0317277728fb918f28dc795a2ce2
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2026-09-17 14:34:27 +0200

    create model for menu items

--- 01-ios-foundations/03-restaurant-app/RestaurantApp/RestaurantApp/Models/MenuItem.swift
+++ 01-ios-foundations/03-restaurant-app/RestaurantApp/RestaurantApp/Models/MenuItem.swift
@@ -7,3 +7,14 @@
 
 import Foundation
 
+/// A suhsi menu item.
+struct MenuItem: Identifiable {
+    /// A unique identifier for each sushi item.
+    let id: UUID = .init()
+    /// The name to represent the sushi dish.
+    let name: String
+    /// The price of sushi dish in dollars rounded to two decimal digits.
+    let price: String
+    /// The image name of the sushi dish in the Assets catalog.
+    let imageName: String
+}
