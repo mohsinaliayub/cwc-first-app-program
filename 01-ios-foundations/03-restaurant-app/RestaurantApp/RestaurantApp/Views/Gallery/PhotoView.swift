c8accbbedda4ac1973cd2a6a1160f93aaa5365f4
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2026-09-18 02:31:18 +0200

    display image in modal sheet and dismiss it

--- 01-ios-foundations/03-restaurant-app/RestaurantApp/RestaurantApp/Views/Gallery/PhotoView.swift
+++ 01-ios-foundations/03-restaurant-app/RestaurantApp/RestaurantApp/Views/Gallery/PhotoView.swift
@@ -9,9 +9,32 @@ import SwiftUI
 
 struct PhotoView: View {
     let galleryItem: GalleryItem
+    @Environment(\.dismiss) private var dismiss
     
     var body: some View {
-        Text("Hello, World!")
+        ZStack(alignment: .trailing) {
+            Image(galleryItem.imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            dismissButton
+        }
+    }
+    
+    private var dismissButton: some View {
+        VStack {
+            Button {
+                dismiss()
+            } label: {
+                Image(systemName: "x.circle")
+                    .scaleEffect(2)
+                    .foregroundStyle(.black)
+            }
+            .padding()
+            .padding(.top)
+            
+            Spacer()
+        }
     }
 }
 
