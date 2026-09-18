cca3a72eca26e731feca8b61deb36c113918cae1
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2026-09-18 02:20:01 +0200

    create stub for gallery item view

---
+++ 01-ios-foundations/03-restaurant-app/RestaurantApp/RestaurantApp/Views/Gallery/PhotoView.swift
@@ -0,0 +1,20 @@
+//
+//  PhotoView.swift
+//  RestaurantApp
+//
+//  Created by Mohsin Ali Ayub on 18.09.26.
+//
+
+import SwiftUI
+
+struct PhotoView: View {
+    let galleryItem: GalleryItem
+    
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    PhotoView(galleryItem: GalleryItem(imageName: "gallery7"))
+}
