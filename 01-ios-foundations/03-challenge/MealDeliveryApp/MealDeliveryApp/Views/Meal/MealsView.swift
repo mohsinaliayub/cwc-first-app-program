21b85317def972346c99e5c78d7928b5c0b6ecdc
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2026-09-18 03:58:31 +0200

    display meal details when tapped in meals screen

--- 01-ios-foundations/03-challenge/MealDeliveryApp/MealDeliveryApp/Views/Meal/MealsView.swift
+++ 01-ios-foundations/03-challenge/MealDeliveryApp/MealDeliveryApp/Views/Meal/MealsView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct MealsView: View {
     @State private var meals: [Meal] = []
+    @State private var selectedMeal: Meal?
     private let columns = Array(repeating: GridItem(spacing: 12), count: 2)
     
     var body: some View {
@@ -24,6 +25,9 @@ struct MealsView: View {
                                 .aspectRatio(2/1.5, contentMode: .fill)
                                 .frame(maxWidth: width > 0 ? (width - 44) / 2 : .zero)
                                 .clipShape(RoundedRectangle(cornerRadius: 8))
+                                .onTapGesture {
+                                    selectedMeal = meal
+                                }
                         }
                     }
                     .padding(.horizontal)
@@ -34,11 +38,49 @@ struct MealsView: View {
                     let dataService = DataService()
                     meals = dataService.fetchMeals()
                 }
+                .sheet(item: $selectedMeal) { meal in
+                    MealView(meal: meal)
+                }
             }
+        }
+    }
+}
+
+struct MealView: View {
+    let meal: Meal
+    @Environment(\.dismiss) private var dismiss
+    
+    var body: some View {
+        ZStack(alignment: .trailing) {
+            Image(meal.imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            dismissButton
+        }
+    }
+    
+    var dismissButton: some View {
+        VStack {
+            Button {
+                dismiss()
+            } label: {
+                Image(systemName: "x.circle")
+                    .scaleEffect(2)
+                    .foregroundStyle(.black)
+            }
+            .padding()
+            .padding(.top)
+            
+            Spacer()
         }
     }
 }
 
 #Preview {
     MealsView()
+}
+
+#Preview {
+    MealView(meal: Meal(imageName: "3"))
 }
