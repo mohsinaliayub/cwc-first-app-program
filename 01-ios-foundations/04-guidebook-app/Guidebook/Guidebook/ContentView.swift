ab923f91b9c76caae4dab34feeb61df72750d31e
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2026-09-18 04:17:16 +0200

    create starter project for Guidebook

---
+++ 01-ios-foundations/04-guidebook-app/Guidebook/Guidebook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Guidebook
+//
+//  Created by Mohsin Ali Ayub on 18.09.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 01-ios-foundations/04-guidebook-app/Guidebook/Guidebook/GuidebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuidebookApp.swift
+//  Guidebook
+//
+//  Created by Mohsin Ali Ayub on 18.09.26.
+//
+
+import SwiftUI
+
+@main
+struct GuidebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
