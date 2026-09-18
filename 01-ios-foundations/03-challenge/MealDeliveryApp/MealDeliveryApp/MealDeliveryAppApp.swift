aef424bc8eec2c675d65d544d9383a1839a97e28
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2026-09-18 03:09:16 +0200

    create starter project for meal delivery app challenge

---
+++ 01-ios-foundations/03-challenge/MealDeliveryApp/MealDeliveryApp/MealDeliveryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealDeliveryAppApp.swift
+//  MealDeliveryApp
+//
+//  Created by Mohsin Ali Ayub on 18.09.26.
+//
+
+import SwiftUI
+
+@main
+struct MealDeliveryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
