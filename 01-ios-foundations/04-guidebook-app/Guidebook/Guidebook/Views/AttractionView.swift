4fd69040f4a0cfb2e052e695444ccc056033b284
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2026-09-18 18:21:53 +0200

    finish ui of AttractionView and fix the issue of text overflow when image is set to fill

--- 01-ios-foundations/04-guidebook-app/Guidebook/Guidebook/Views/AttractionView.swift
+++ 01-ios-foundations/04-guidebook-app/Guidebook/Guidebook/Views/AttractionView.swift
@@ -11,19 +11,34 @@ struct AttractionView: View {
     let attraction: Attraction
     
     var body: some View {
-        ScrollView {
-            VStack(alignment: .leading) {
+        GeometryReader { proxy in
+            VStack(alignment: .leading, spacing: 10) {
                 Image(attraction.imageName)
                     .resizable()
-                    .aspectRatio(contentMode: .fit)
-                Text(attraction.name)
+                    .aspectRatio(contentMode: .fill)
+                    .frame(height: 300)
+                
+                
+                ScrollView {
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text(attraction.name)
+                            .font(.title)
+                            .bold()
+                        Text(attraction.longDescription)
+                            .multilineTextAlignment(.leading)
+                    }
+                    .frame(maxWidth: proxy.size.width)
+                    .padding(.bottom, 30)
+                    .padding(.horizontal)
+                }
+                .scrollIndicators(.hidden)
             }
-            .padding()
+            .ignoresSafeArea()
         }
     }
 }
 
 #Preview {
-    let attraction = DataService.fetchPreviewCity().attractions[0]
+    let attraction = DataService.fetchPreviewCity().attractions[1]
     AttractionView(attraction: attraction)
 }
