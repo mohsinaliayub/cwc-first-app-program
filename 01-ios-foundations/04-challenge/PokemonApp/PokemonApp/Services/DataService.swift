85e3415676943541a5d43fd2db8bea3d248d01f9
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2026-09-19 20:12:26 +0200

    add a method to fetch a category for swiftui preview

--- 01-ios-foundations/04-challenge/PokemonApp/PokemonApp/Services/DataService.swift
+++ 01-ios-foundations/04-challenge/PokemonApp/PokemonApp/Services/DataService.swift
@@ -28,4 +28,48 @@ struct DataService {
         
         return []
     }
+    
+    /// A dummy category to work with SwiftUI views.
+    func previewCategory() -> PokemonCategory {
+        let pokemons = [
+            Pokemon(name: "Pikachu",
+                    summary: "When it is angered, it immediately discharges the energy stored in the pouches in its cheeks.",
+                    hp: 3,
+                    attack: 4,
+                    defense: 3,
+                    specialAttack: 3,
+                    specialDefense: 3,
+                    speed: 6,
+                    imageName: "electric-pikachu"),
+            Pokemon(name: "Chinchou",
+                    summary: "It lives in ocean depths beyond the reach of sunlight. It flashes lights on its antennae to communicate with others of its kind.",
+                    hp: 5,
+                    attack: 3,
+                    defense: 3,
+                    specialAttack: 4,
+                    specialDefense: 4,
+                    speed: 4,
+                    imageName: "electric-chinchou"),
+            Pokemon(name: "Zapdos",
+                    summary: "This legendary Pokémon is said to live in thunderclouds. It freely controls lightning bolts.",
+                    hp: 6,
+                    attack: 6,
+                    defense: 5,
+                    specialAttack: 8,
+                    specialDefense: 6,
+                    speed: 6,
+                    imageName: "electric-zapdos"),
+            Pokemon(name: "Jolteon",
+                    summary: "It concentrates the weak electric charges emitted by its cells and launches wicked lightning bolts.",
+                    hp: 4,
+                    attack: 4,
+                    defense: 4,
+                    specialAttack: 6,
+                    specialDefense: 5,
+                    speed: 7,
+                    imageName: "electric-jolteon"),
+        ]
+        
+        return PokemonCategory(name: "Electric", pokemons: pokemons)
+    }
 }
