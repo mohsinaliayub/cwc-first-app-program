3a0921bf2001392f4a3d99be3a1047f4b5f30ba5
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2026-09-19 19:49:32 +0200

    rename main view and add data service

---
+++ 01-ios-foundations/04-challenge/PokemonApp/PokemonApp/Services/DataService.swift
@@ -0,0 +1,8 @@
+//
+//  DataService.swift
+//  PokemonApp
+//
+//  Created by Mohsin Ali Ayub on 19.09.26.
+//
+
+import Foundation
--- 01-ios-foundations/04-challenge/PokemonApp/PokemonApp/Views/CategoriesView.swift
+++ 01-ios-foundations/04-challenge/PokemonApp/PokemonApp/Views/CategoriesView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  CategoriesView.swift
 //  PokemonApp
 //
 //  Created by Mohsin Ali Ayub on 19.09.26.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct CategoriesView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    CategoriesView()
 }
