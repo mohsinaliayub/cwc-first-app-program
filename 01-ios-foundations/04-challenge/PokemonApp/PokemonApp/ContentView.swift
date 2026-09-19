a5c94ee5cffd137721bdf9048233def2eb271d76
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2026-09-19 19:42:52 +0200

    create starter project for Pokemon app

---
+++ 01-ios-foundations/04-challenge/PokemonApp/PokemonApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PokemonApp
+//
+//  Created by Mohsin Ali Ayub on 19.09.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 01-ios-foundations/04-challenge/PokemonApp/PokemonApp/PokemonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonAppApp.swift
+//  PokemonApp
+//
+//  Created by Mohsin Ali Ayub on 19.09.26.
+//
+
+import SwiftUI
+
+@main
+struct PokemonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
