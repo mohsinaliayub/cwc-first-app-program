f528b0f46d045af2c4b759fa497c3bc95b89e581
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2026-09-19 20:26:32 +0200

    display pokemon categories in SwiftUI

--- 01-ios-foundations/04-challenge/PokemonApp/PokemonApp/Views/CategoriesView.swift
+++ 01-ios-foundations/04-challenge/PokemonApp/PokemonApp/Views/CategoriesView.swift
@@ -8,18 +8,79 @@
 import SwiftUI
 
 struct CategoriesView: View {
+    @State private var pokemonCategories: [PokemonCategory] = []
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ScrollView {
+                categoriesView
+                    .padding(.horizontal)
+            }
+            .scrollIndicators(.hidden)
+            .navigationTitle("Categories")
+            .onAppear {
+                let dataService = DataService()
+                pokemonCategories = dataService.fetchPokemonCategories()
+            }
         }
-        .padding()
-        .onAppear {
-            let dataService = DataService()
-            let categories = dataService.fetchPokemonCategories()
-            print(categories.count)
+    }
+    
+    private var categoriesView: some View {
+        VStack(spacing: 12) {
+            ForEach(pokemonCategories) { category in
+                CategoryRow(for: category)
+                    .foregroundStyle(.black)
+            }
+        }
+    }
+}
+
+struct CategoryRow: View {
+    let category: PokemonCategory
+    
+    init(for pokemonCategory: PokemonCategory) {
+        self.category = pokemonCategory
+    }
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 12)
+            .fill(color)
+            .frame(height: 60)
+            .overlay {
+                HStack {
+                    Image(systemName: iconName)
+                    Text(category.name).font(.headline)
+                }
+            }
+    }
+    
+    private var iconName: String {
+        switch (category.name) {
+        case "Electric":
+            return "bolt.circle"
+        case "Fire":
+            return "flame.circle"
+        case "Grass":
+            return "leaf.circle"
+        case "Water":
+            return "drop.circle"
+        default:
+            return ""
+        }
+    }
+    
+    private var color: Color {
+        switch (category.name) {
+        case "Electric":
+            return Color.yellow
+        case "Fire":
+            return Color.red
+        case "Grass":
+            return Color.green
+        case "Water":
+            return Color.blue
+        default:
+            return Color.gray
         }
     }
 }
