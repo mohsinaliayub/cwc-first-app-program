57521f0961b09fb9925ea6f4b77a35c3d68067d6
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2026-09-21 13:53:14 +0200

    add starter project or city sights

---
+++ 02-ios-networking/01-city-sights/CitySights/CitySights/CitySightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySightsApp.swift
+//  CitySights
+//
+//  Created by Mohsin Ali Ayub on 21.09.26.
+//
+
+import SwiftUI
+
+@main
+struct CitySightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
