00c618703c3f7bfd4ba0426b0aca87e4bb090748
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2026-09-21 17:33:33 +0200

    display businesses from search request

--- 02-ios-networking/01-city-sights/CitySights/CitySights/Views/MainView.swift
+++ 02-ios-networking/01-city-sights/CitySights/CitySights/Views/MainView.swift
@@ -10,23 +10,36 @@ import SwiftUI
 struct MainView: View {
     @State private var query = ""
     @State private var businesses: [Business] = []
+    private let dataService = DataService()
     
     var body: some View {
+        VStack {
+            searchBar
+            businessesListView
+        }
+        .padding()
+        .task {
+            businesses = await dataService.searchRestaurants()
+        }
+    }
+    
+    private var searchBar: some View {
         HStack {
             TextField("What're you looking for?", text: $query)
                 .textFieldStyle(.roundedBorder)
             Button {
                 // TODO: Implement query search
-                Task {
-                    let dataService = DataService()
-                    businesses = await dataService.searchRestaurants()
-                    print(businesses.count)
-                }
             } label: {
                 Text("Go")
             }
         }
-        .padding()
+    }
+    
+    private var businessesListView: some View {
+        List(businesses) { business in
+            Text(business.name)
+        }
+        .listStyle(.plain)
     }
 }
 
