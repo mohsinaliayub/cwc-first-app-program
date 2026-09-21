63d843c79ef6d19153c17c402aa1c4aded3c407b
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2026-09-21 16:52:49 +0200

    create network request to fetch local restaurants

---
+++ 02-ios-networking/01-city-sights/CitySights/CitySights/Services/DataService.swift
@@ -0,0 +1,42 @@
+//
+//  DataService.swift
+//  CitySights
+//
+//  Created by Mohsin Ali Ayub on 21.09.26.
+//
+
+import Foundation
+
+struct DataService {
+    private let apiKey = Bundle.main.infoDictionary?["API_KEY"] as? String
+    private let latitude = 35.665517
+    private let longitude = 139.770398
+    
+    /// Search local restaurants based on your location.
+    func searchRestaurants() async {
+        // If api key doesn't exist, return.
+        guard let apiKey else { return }
+        
+        guard let url = URL(string: "https://api.yelp.com/v3/businesses/search?categories=restaurants&latitude=\(latitude)&longitude=\(longitude)&limit=10") else {
+            return
+        }
+        
+        // Create URL Request
+        var request = URLRequest(url: url)
+        request.addValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
+        request.addValue("application/json", forHTTPHeaderField: "accept")
+        
+        // Send request
+        do {
+            let (data, response) = try await URLSession.shared.data(for: request)
+            
+            // Check if response is successful
+            guard (response as? HTTPURLResponse)?.statusCode == 200 else { return }
+            
+            // Parse the JSON data
+            print(data)
+        } catch {
+            print(error)
+        }
+    }
+}
