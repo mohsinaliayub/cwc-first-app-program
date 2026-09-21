3d3b1e57fd81ccf18670b4ec0539251038ac2bcb
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2026-09-21 17:43:46 +0200

    write documentation for Location data model

--- 02-ios-networking/01-city-sights/CitySights/CitySights/Models/Location.swift
+++ 02-ios-networking/01-city-sights/CitySights/CitySights/Models/Location.swift
@@ -7,14 +7,23 @@
 
 import Foundation
 
+/// An immutable representation of the postal address for a business.
 struct Location {
+    /// The street name in a postal address.
     let address1: String?
+    /// Additional information associated with the location, typically defined at the street level, in a postal address.
     let address2: String?
+    /// Additional information associated with the location, typically defined at the city or town level, in a postal address.
     let address3: String?
+    /// The city name in a postal address.
     let city: String?
+    /// The country or region name in a postal address.
     let country: String?
+    /// The full postal address of the business, separated by parts in an array of strings.
     let displayAddress: [String]?
+    /// The state name in a postal address.
     let state: String?
+    /// The postal code in a postal address.
     let zipCode: String?
 }
 
