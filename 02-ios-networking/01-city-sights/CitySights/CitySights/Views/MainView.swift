d374748ac993ee16333efa19e7008a6df8f71ef7
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2026-09-21 15:48:00 +0200

    add query text field and button

--- 02-ios-networking/01-city-sights/CitySights/CitySights/Views/MainView.swift
+++ 02-ios-networking/01-city-sights/CitySights/CitySights/Views/MainView.swift
@@ -8,12 +8,17 @@
 import SwiftUI
 
 struct MainView: View {
+    @State private var query = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            TextField("What're you looking for?", text: $query)
+                .textFieldStyle(.roundedBorder)
+            Button {
+                // TODO: Implement query search
+            } label: {
+                Text("Go")
+            }
         }
         .padding()
     }
