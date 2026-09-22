072239f1f4ee03789f51fd9d57fedefa1e01e072
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2026-09-22 02:54:01 +0200

    create documentation for BusinessSearch

--- 02-ios-networking/01-city-sights/CitySights/CitySights/Models/BusinessSearch.swift
+++ 02-ios-networking/01-city-sights/CitySights/CitySights/Models/BusinessSearch.swift
@@ -7,9 +7,13 @@
 
 import Foundation
 
+/// Represents a local business search request on Yelp.
 struct BusinessSearch {
+    /// A collection of businesses matching your request.
     let businesses: [Business]
+    /// The origin of the search request.
     let region: Region
+    /// The total number of local businesses found with your request.
     let total: Int
 }
 
