3e397a2e1dc8cf9300aa618ead278038428b3cc7
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2026-09-23 16:16:49 +0200

    create MapView to show businesses marker

---
+++ 02-ios-networking/01-city-sights/CitySights/CitySights/Views/Map/MapView.swift
@@ -0,0 +1,19 @@
+//
+//  MapView.swift
+//  CitySights
+//
+//  Created by Mohsin Ali Ayub on 23.09.26.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    var body: some View {
+        Map()
+    }
+}
+
+#Preview {
+    MapView()
+}
