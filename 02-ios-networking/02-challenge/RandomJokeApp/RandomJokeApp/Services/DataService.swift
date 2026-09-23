1f8c46447fe037769cf3d319d5c748d683eeed7e
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2026-09-23 17:29:52 +0200

    fetch and display a random joke

--- 02-ios-networking/02-challenge/RandomJokeApp/RandomJokeApp/Services/DataService.swift
+++ 02-ios-networking/02-challenge/RandomJokeApp/RandomJokeApp/Services/DataService.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+struct DataService {
+    private let endPoint = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single"
+    
+    func fetchRandomJoke() async -> Joke? {
+        guard let url = URL(string: endPoint) else { return nil }
+        
+        do {
+            let (data, response) = try await URLSession.shared.data(from: url)
+            
+            guard (response as? HTTPURLResponse)?.statusCode == 200 else { return nil }
+            
+            let joke = try JSONDecoder().decode(Joke.self, from: data)
+            return joke            
+        } catch {
+            print(error)
+        }
+        
+        return nil
+    }
+}
--- 02-ios-networking/02-challenge/RandomJokeApp/RandomJokeApp/Views/ContentView.swift
+++ 02-ios-networking/02-challenge/RandomJokeApp/RandomJokeApp/Views/ContentView.swift
@@ -8,19 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var joke: Joke?
+    @State private var error: String?
+    private let service = DataService()
+    
     var body: some View {
         ZStack {
-            Text("If Bill Gates had a dime for every time Windows crashed ... Oh wait, he does.")
+            Text(joke?.text ?? error ?? "Click Button to Get a Random Joke")
             
             VStack {
                 Spacer()
                 
                 Button("Get Random Joke") {
-                    
+                    fetchJoke()
                 }
             }
         }
         .padding()
+    }
+    
+    private func fetchJoke() {
+        Task {
+            if let joke = await service.fetchRandomJoke() {
+                self.joke = joke
+            } else {
+                error = "Couldn't find the joke. Please try again..."
+            }
+        }
     }
 }
 
