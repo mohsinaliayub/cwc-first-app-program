21927ad1e2dd2474b50975b8ba4c5b24b8028c23
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2026-09-23 16:58:12 +0200

    create starter project for Jokes app

---
+++ 02-ios-networking/02-challenge/RandomJokeApp/RandomJokeApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RandomJokeApp
+//
+//  Created by Mohsin Ali Ayub on 23.09.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 02-ios-networking/02-challenge/RandomJokeApp/RandomJokeApp/RandomJokeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomJokeAppApp.swift
+//  RandomJokeApp
+//
+//  Created by Mohsin Ali Ayub on 23.09.26.
+//
+
+import SwiftUI
+
+@main
+struct RandomJokeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
