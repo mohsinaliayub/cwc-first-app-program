3f0703a7968feab3abb5f9dbd0704da51dc4adda
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2026-09-24 01:11:32 +0200

    create starter project for football stats

---
+++ 02-ios-networking/03-challenge/FootballStats/FootballStats/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FootballStats
+//
+//  Created by Mohsin Ali Ayub on 24.09.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 02-ios-networking/03-challenge/FootballStats/FootballStats/FootballStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballStatsApp.swift
+//  FootballStats
+//
+//  Created by Mohsin Ali Ayub on 24.09.26.
+//
+
+import SwiftUI
+
+@main
+struct FootballStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
