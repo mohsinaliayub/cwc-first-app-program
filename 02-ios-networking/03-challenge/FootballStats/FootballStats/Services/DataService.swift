a3842434b53a5c54d70cb2b1d72249044830ceda
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2026-09-24 01:28:19 +0200

    fetch football teams from api

--- 02-ios-networking/03-challenge/FootballStats/FootballStats/Services/DataService.swift
+++ 02-ios-networking/03-challenge/FootballStats/FootballStats/Services/DataService.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+struct DataService {
+    private let apiKey = Bundle.main.infoDictionary?["API_KEY"] as? String
+    private let teamsEndpoint = "https://api.football-data.org/v4/teams"
+    
+    /// Fetch a collection of football teams.
+    func fetchFootballTeams() async -> [FootballTeam] {
+        guard let apiKey, let url = URL(string: teamsEndpoint) else { return [] }
+        
+        var request = URLRequest(url: url)
+        request.addValue(apiKey, forHTTPHeaderField: "X-Auth-Token")
+        
+        do {
+            let (data, response) = try await URLSession.shared.data(for: request)
+            
+            guard (response as? HTTPURLResponse)?.statusCode == 200 else { return [] }
+            
+            let teamsData = try JSONDecoder().decode(FootballTeamsResult.self, from: data)
+            return teamsData.teams
+        } catch {
+            print(error)
+        }
+        
+        return []
+    }
+}
