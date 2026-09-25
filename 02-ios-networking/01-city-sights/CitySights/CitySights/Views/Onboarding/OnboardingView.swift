b77b16daf39a0f282686857ebcdea6a1ff7dbde3
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2026-09-25 09:47:30 +0200

    hide the white background when user swipes the edge of the screen

--- 02-ios-networking/01-city-sights/CitySights/CitySights/Views/Onboarding/OnboardingView.swift
+++ 02-ios-networking/01-city-sights/CitySights/CitySights/Views/Onboarding/OnboardingView.swift
@@ -8,12 +8,27 @@
 import SwiftUI
 
 struct OnboardingView: View {
+    private let blueColor = Color(red: 111/255, green: 154/255, blue: 189/255)
+    private let greenColor = Color(red: 139/255, green: 166/255, blue: 65/255)
     @State private var selectedViewIndex = 0
     @Environment(\.dismiss) var dismiss
     
     var body: some View {
+        ZStack {
+            if selectedViewIndex == 0 {
+                blueColor
+            } else {
+                greenColor
+            }
+            
+            pagedTabView
+        }
+        .ignoresSafeArea()
+    }
+    
+    private var pagedTabView: some View {
         TabView(selection: $selectedViewIndex) {
-            OnboardingScene(bgColor: Color(red: 111/255, green: 154/255, blue: 189/255),
+            OnboardingScene(bgColor: blueColor,
                             headline: "Welcome to City Sights",
                             subheadline: "City Sights helps you find the best of the city!") {
                 withAnimation {
@@ -23,7 +38,7 @@ struct OnboardingView: View {
                             .ignoresSafeArea()
                             .tag(0)
             
-            OnboardingScene(bgColor: Color(red: 139/255, green: 166/255, blue: 65/255),
+            OnboardingScene(bgColor: greenColor,
                             headline: "Discover your City",
                             subheadline: "We'll show you the best restaurants, venues, and more, based on your location.") {
                 dismiss()
@@ -31,8 +46,7 @@ struct OnboardingView: View {
                             .ignoresSafeArea()
                             .tag(1)
         }
-        .tabViewStyle(.page)
-        .ignoresSafeArea()
+        .tabViewStyle(.page(indexDisplayMode: .never))
     }
 }
 
