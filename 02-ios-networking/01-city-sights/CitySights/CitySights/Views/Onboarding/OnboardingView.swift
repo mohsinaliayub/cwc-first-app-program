671e893aac2f41884e1b781f9f6678ea4624eaac
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2026-09-25 08:42:05 +0200

    add onboarding view starter file

---
+++ 02-ios-networking/01-city-sights/CitySights/CitySights/Views/Onboarding/OnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingView.swift
+//  CitySights
+//
+//  Created by Mohsin Ali Ayub on 25.09.26.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
