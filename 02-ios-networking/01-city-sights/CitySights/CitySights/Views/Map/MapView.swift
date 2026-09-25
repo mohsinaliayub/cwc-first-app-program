d52be1ee88c0c2155b1d635b57146f9ccf4dd698
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2026-09-25 08:09:31 +0200

    prepare MapView to display business map markers

--- 02-ios-networking/01-city-sights/CitySights/CitySights/Views/Map/MapView.swift
+++ 02-ios-networking/01-city-sights/CitySights/CitySights/Views/Map/MapView.swift
@@ -9,11 +9,16 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
+    @Environment(BusinessViewModel.self) var model
+    
     var body: some View {
-        Map()
+        Map() {
+            
+        }
     }
 }
 
 #Preview {
     MapView()
+        .environment(BusinessViewModel(dataService: DataService()))
 }
