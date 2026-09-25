403f84f7f5b912c95f53c1acd2a5b3912f7f4377
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2026-09-25 08:23:00 +0200

    select a business marker in map view

--- 02-ios-networking/01-city-sights/CitySights/CitySights/Views/Map/MapView.swift
+++ 02-ios-networking/01-city-sights/CitySights/CitySights/Views/Map/MapView.swift
@@ -10,13 +10,18 @@ import MapKit
 
 struct MapView: View {
     @Environment(BusinessViewModel.self) var model
+    @State private var selectedBusinessID: String?
     
     var body: some View {
-        Map() {
+        Map(selection: $selectedBusinessID) {
             ForEach(model.businesses) { business in
                 Marker(business.name, coordinate: CLLocationCoordinate2D(latitude: business.coordinates?.latitude ?? 0,
                                                                          longitude: business.coordinates?.longitude ?? 0))
+                .tag(business.id)
             }
+        }
+        .onChange(of: selectedBusinessID) { oldValue, newValue in
+            // Find the matching business
         }
     }
 }
