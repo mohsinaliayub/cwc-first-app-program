c39d8bff9bc7a397dc45bfae03b168599d1bdfac
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2026-09-25 09:04:16 +0200

    create one scene of onboarding view

--- 02-ios-networking/01-city-sights/CitySights/CitySights/Views/Onboarding/OnboardingView.swift
+++ 02-ios-networking/01-city-sights/CitySights/CitySights/Views/Onboarding/OnboardingView.swift
@@ -10,11 +10,45 @@ import SwiftUI
 struct OnboardingView: View {
     var body: some View {
         TabView {
-            Rectangle().fill(.red)
-            
-            Rectangle().fill(.green)
+            ZStack {
+                Color(red: 111/255, green: 154/255, blue: 189/255)
+                VStack(spacing: 0) {
+                    Spacer()
+                    Spacer()
+                    
+                    Image("onboarding")
+                        .padding(.bottom, 32)
+                    
+                    Text("Welcome to City Sights")
+                        .font(.title2)
+                        .bold()
+                        .padding(.bottom, 4)
+                    
+                    Text("City Sights helps you find the best of the city!")
+                    
+                    Spacer()
+                    
+                    Button {
+                        // TODO
+                    } label: {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 14)
+                                .foregroundStyle(.white)
+                            Text("Continue")
+                                .foregroundStyle(.black)
+                                .bold()
+                        }
+                    }
+                    .frame(height: 50)
+                    .padding(.horizontal)
+                    .padding(.bottom, 115)
+                }
+                .foregroundStyle(.white)
+            }
+            .ignoresSafeArea()
         }
         .tabViewStyle(.page)
+        .ignoresSafeArea()
     }
 }
 
